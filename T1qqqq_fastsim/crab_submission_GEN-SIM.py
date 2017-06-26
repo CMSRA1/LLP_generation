fcfa5d7e8b1705fa9b9b4a1a4076e744c4508aeb
Shane Breeze <s.breeze15@imperial.ac.uk>
2017-06-26 13:27:27 +0200

    Add crab submission files for T1qqqq fast sim

---
+++ T1qqqq_fastsim/crab_submission_GEN-SIM.py
@@ -0,0 +1,22 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName     = 'T1qqqq'
+config.General.workArea        = 'T1qqqq_GEN-SIM'
+config.General.transferOutputs = True
+config.General.transferLogs    = True
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName   = 'step1_T1qqqq_AODSIM_cfg.py'
+
+config.Data.outputPrimaryDataset = 'T1qqqq_fastsim_Madgraph_Pythia'
+config.Data.inputDBS             = 'global'
+config.Data.splitting            = 'EventBased'
+config.Data.unitsPerJob          = 500
+config.Data.totalUnits           = 100000
+config.Data.outLFNDirBase        = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication          = True
+config.Data.outputDatasetTag     = 'T1qqqq_fastsim_Madgraph_Pythia_test_GEN-SIM'
+
+config.Site.storageSite = 'T2_UK_London_IC'
+#config.Site.storageSite = 'T2_UK_SGrid_Bristol'
